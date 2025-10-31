72091c0d11a5b935e5a801f72077e3b5581a4208
Vaibhav Rathod <vaibhav.rathod@nsvtech.com>
2025-10-31 17:47:21 +0530

    added math_tool

---
+++ Module2/assignment/file_finder/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+function createTempFiles() {
+
+    fs.writeFileSync('./demo_folder/file1.txt', 'Contents of file 1')
+    fs.writeFileSync('./demo_folder/file2.txt', 'Contents of file 2')
+    fs.writeFileSync('./demo_folder/file3.txt', 'Contents of file 3')
+
+    console.log('Files created successfully.')
+} 
+
+function readFiles() {
+    
+    const folder_name = './demo_folder';
+    fs.readdir(folder_name, (err, files) => {
+          
+        if(err) {
+            console.log("Error while reading the files.");
+            return;
+        } 
+        
+        console.log("Below are the file names in demo_folder :");
+        files.forEach(file => {
+            console.log(file+ " ");
+        })
+         
+    });
+
+
+}
+
+createTempFiles();
+setTimeout(() => {
+    readFiles()
+}, 0)
